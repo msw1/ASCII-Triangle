1b8e732ee46cca177a078ae098813a904d620e00
msw1 <mattitron52@gmail.com>
2014-10-11 18:15:21 -0700

    Remove doEitherIf

--- Triangle.hs
+++ Triangle.hs
@@ -1,6 +1,6 @@
 module Triangle (triangle) where
 import Shortener
--- This function returns an ASCII triangle.
+
 makeTriangle :: Int -> Int -> Int -> Int -> String
 makeTriangle height counter indent width =
     if counter <= height
@@ -10,21 +10,20 @@ makeTriangle height counter indent width =
           ++ makeTriangle height incCounter incIndent incWidth
         else ""
       where
-        newIndent  = doEitherIf (id) (subtract a) (height > 0) indent
-        incIndent  = doEitherIf (+1) (subtract 1) (height < 0) indent
-        incWidth   = doEitherIf (+2) (subtract 2) (height > 0) width
+        newIndent  = if height > 0 then indent     else indent - a
+        incIndent  = if height < 0 then indent + 1 else indent - 1 
+        incWidth   = if height > 0 then width  + 2 else width  - 2
         incCounter = counter + 1
         -- Subtract for every time we're going to add to the indent.
         a          = abs(height) - 1
 
--- This function creates the triangle's variables.
 triangle :: Int -> Int -> String
-triangle height termWidth
+triangle height tWidth
     -- If the width would overflow, use the max that won't.
-    | abs height > halfTermWidth = triangle newHeight termWidth
-    | otherwise = makeTriangle height counter halfTermWidth width
+    | abs height > hTWidth = triangle newHeight tWidth
+    | otherwise = makeTriangle height counter hTWidth width
       where
-        halfTermWidth = termWidth `div` 2
-        width         = if height > 0 then 1 else (-height) * 2 - 1
-        newHeight     = doEitherIf (id) (negate)    (height > 0) halfTermWidth
-        counter       = doEitherIf (+1) (+height*2) (height > 0) 0
+        hTWidth   = tWidth `div` 2
+        width     = if height > 0 then 1 else (-height) * 2 - 1
+        counter   = if height > 0 then 1 else (height * 2)
+        newHeight = if height > 0 then hTWidth else -hTWidth
